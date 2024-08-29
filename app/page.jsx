e00fbc1389581cfe2b67f7f674d9a4b54b0d9286
Vraj Roy <vrajroy2k11@gmail.com>
2024-08-29 12:07:11 +0530

    home page next page button missing fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import News from "./components/News";
 import Loading from "./loading";
+import Link from "next/link";
 const Home = async () => {
   const apiKey = process.env.NEWS_API_KEY;
 
@@ -10,23 +11,33 @@ const Home = async () => {
   const news = await res.json();
 
   return (
-    <Suspense fallback={<Loading />}>
-      <div className="flex m-auto md:w-[80vw] justify-center md:justify-between p-4 flex-wrap mt-10">
-        {news.data.map((d) => (
-          <News
-            key={d.uuid}
-            uuid={d.uuid}
-            title={d.title}
-            description={d.description}
-            snippet={d.snippet}
-            url={d.url}
-            image_url={d.image_url}
-            source={d.source}
-            published_at={d.published_at}
-          />
-        ))}
+    <>
+      <Suspense fallback={<Loading />}>
+        <div className="flex m-auto md:w-[80vw] justify-center md:justify-between p-4 flex-wrap mt-10">
+          {news.data.map((d) => (
+            <News
+              key={d.uuid}
+              uuid={d.uuid}
+              title={d.title}
+              description={d.description}
+              snippet={d.snippet}
+              url={d.url}
+              image_url={d.image_url}
+              source={d.source}
+              published_at={d.published_at}
+            />
+          ))}
+        </div>
+      </Suspense>
+      <div className="flex justify-between md:w-[80vw] md:m-auto mt-10 m-4 text-sm md:text-lg">
+        <div></div>
+        <Link href={"/page/2"}>
+          <div className="btn bg-black text-white p-4 w-fit rounded mt-4">
+            Next Page
+          </div>
+        </Link>
       </div>
-    </Suspense>
+    </>
   );
 };
 
